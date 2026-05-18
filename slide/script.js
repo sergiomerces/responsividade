157dfa146779dc02ece0ae6fec1605cd5a050ffe
sergiomerces <sergio.merces@gmail.com>
2026-05-17 21:32:59 -0300

    carrosel de imagens responsivo

---
+++ slide/script.js
@@ -0,0 +1,30 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function changeSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("carousel-slide");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+//auto slide change every 5 seconds
+setInterval(() => {
+    showSlides(slideIndex += 1);
+}, 5000);
